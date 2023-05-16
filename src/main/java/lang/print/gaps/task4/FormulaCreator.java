be3d69b25599c16a2872c5d51264ec69d636f310
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-16 19:43:50 +0600

    mod3task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 1;
+        double b = 1;
+        System.out.println((9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2);
     }
 }
